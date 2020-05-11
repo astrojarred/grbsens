ff3531fcf93e2a9d6e49bb9144ce0f03b1f152e0
Jarred Green <astrojarred@gmail.com>
2020-05-11 18:11:25 +0200

    Initial commit

---
+++ grbsens/grb.py
@@ -0,0 +1,9 @@
+class grb():
+
+    def __init__(numruns=1,
+            numjobs=1,
+             loweren=0.02,
+             upperen=10,
+             bins=1,
+             irf="North_0.5h",
+    ):

21ca0bea98526973a16aacd0d1eb90ddd01beb17
Jarred Green <astrojarred@gmail.com>
2020-07-03 18:21:41 +0200

    Update setup.py.

--- grbsens/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='grbsens',
-      version='0.1',
-      py_modules=['grbsens'],
-      )
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='grbsens',
+      version='0.1',
+      packages=['grbsens']
+      )
